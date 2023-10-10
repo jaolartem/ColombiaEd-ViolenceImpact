3d20bb4c93c7ce03cbdcf81c0e91ba66a06d97c9
jaolartem <110577537+jaolartem@users.noreply.github.com>
2023-10-10 16:41:13 -0500

    Update .gitignore to exclude large directories

--- excel_to_csv.py
+++ excel_to_csv.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-# Read the Excel file and return a dictionary of data frames
-import pandas as pd
-import os
 
 def excel_to_csv(excel):
     """
@@ -29,18 +26,12 @@ def excel_to_csv(excel):
         except Exception as e:
             print(f"Failed to export {csv_file}: {e}")
 
-# Define a function that takes a folder name as an argument and converts all Excel files in that folder to CSV files
-def convert_excel_to_csv(folder_name):
-    # Create the full path of the folder
-    folder_path = os.path.join(os.getcwd(), folder_name)
 
-    # Get the list of the names of the Excel files in the folder
-    excel_list = glob.glob(os.path.join(folder_path, "*.xlsx"))
+def main():
+    # Call the convert_excel_to_csv function with the desired folder name
+    excel_to_csv(excel)
 
-    # Iterate over the list and apply the conversion function
-    for file in excel_list:
-        # Read the Excel file and return a dictionary of data frames
-        df_dict = excel_to_csv(file)
-
-# Call the function with the desired folder name
-
+# Check if the module is being run as the main program or being imported by another module
+if __name__ == "__main__":
+    # Execute the main function only when the module is run as the main program
+    main()
