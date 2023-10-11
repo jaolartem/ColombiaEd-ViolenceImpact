7cb1e58600b335e126d5ba08f9f7f9a36c678a4c
jaolartem <110577537+jaolartem@users.noreply.github.com>
2023-10-11 00:57:09 -0500

    procesing ETL code

--- main_etl.py
+++ main_etl.py
@@ -3,12 +3,13 @@ from pivot_and_save_df import pivot_and_save_df
 
 def main_etl_process():
     folder_name = input("Enter the name of the folder containing Excel files: ")
-    dfs_list = convert_excel_to_csv(folder_name)
+    dfs_dicts = convert_excel_to_csv(folder_name)
     
-    for sheet_name, df in dfs_list:
-        if sheet_name == "MUNICIPIO DEL HECHO":
-            pivot_and_save_df(sheet_name, df)
-            dfs_list[idx] = (sheet_name, pivoted_df)
+    for dfs in dfs_dicts:
+        for sheet_name, df in dfs.items():
+            if sheet_name == "MUNICIPIO DEL HECHO":
+                pivoted_df = pivot_and_save_df(sheet_name, df)
+                dfs[sheet_name] = pivoted_df
 
 if __name__ == "__main__":
     main_etl_process()
--- pivot_and_save_df.py
+++ pivot_and_save_df.py
@@ -1,19 +1,21 @@
-import os
 import pandas as pd
 
-"""
-    Pivots the DataFrame and saves it as a CSV file.
-    
-    Parameters:
-    - df_name (str): The name for the DataFrame (to be used in the CSV file name).
-    - df (DataFrame): The DataFrame to pivot and save.
-    
-    Returns:
-    - DataFrame: The pivoted DataFrame.
-"""
-
 def pivot_and_save_df(sheet_name, dataframe):
+    """
+    Transposes the given dataframe, splits it at the 'Total' column (if present), 
+    and saves each part as a CSV file. The filename is derived from the sheet_name 
+    and the title present in the cell (0,0) of the sub-dataframe.
+
+    Args:
+    - sheet_name (str): Name of the Excel sheet from which the dataframe is derived.
+    - dataframe (pd.DataFrame): DataFrame to be transposed and saved.
+
+    Returns:
+    - dict: Dictionary containing the transformed dataframes.
+    """
     df_pivoted = dataframe.transpose()
+    transformed_dfs = {}
+    
     if 'Total' in df_pivoted.columns:
         idx = df_pivoted.columns.get_loc('Total')
         dfs = [df_pivoted.iloc[:, :idx], df_pivoted.iloc[:, idx+1:]]
@@ -24,14 +26,8 @@ def pivot_and_save_df(sheet_name, dataframe):
             try:
                 sub_dataframe.to_csv(csv_file, index=True, header=True)
                 print(f"Successfully saved pivoted DataFrame as {csv_file}")
+                transformed_dfs[title] = sub_dataframe
             except Exception as e:
                 print(f"Failed to save {csv_file}: {e}")
-
-def main(sheet_name, dataframe):
-    # Call the convert_excel_to_csv function with the desired folder name
-    pivot_and_save_df(sheet_name, dataframe)
-
-# Check if the module is being run as the main program or being imported by another module
-if __name__ == "__main__":
-    # Execute the main function only when the module is run as the main program
-    main()
+                
+    return transformed_dfs
