afd7c8ec5b438fe9cf531e02fe17ec7be9125150
jaolartem <110577537+jaolartem@users.noreply.github.com>
2023-10-11 04:33:05 -0500

    procesing ETL code V3

--- Excel_csv_folder.py
+++ Excel_csv_folder.py
@@ -1,44 +1,22 @@
-import glob
 import os
-import shutil
-from excel_to_csv import excel_to_csv 
+import glob
+from excel_to_csv import excel_to_csv
 
-def move_csv_to_subfolder(folder_path):
-    """Move all .csv files in the specified directory to a subfolder named 'CSV'."""
-    csv_files = glob.glob(os.path.join(folder_path, "*.csv"))
-    
-    # Create 'CSV' subdirectory if it doesn't exist
-    csv_subfolder = os.path.join(folder_path, 'CSV')
-    if not os.path.exists(csv_subfolder):
-        os.makedirs(csv_subfolder)
-        
-    # Move all .csv files to the 'CSV' subdirectory
-    for csv_file in csv_files:
-        shutil.move(csv_file, csv_subfolder)
-    print(f"Moved all CSV files to {csv_subfolder}")
+def folder_to_csv(folder_path):
+    """
+    Convert all Excel files within a specified folder (and its subdirectories) 
+    to CSV format. The CSV files are saved in a backup folder structure 
+    under "CSV's_backout" which mirrors the original directory structure.
 
-def convert_excel_to_csv(folder_name):
-    all_dfs = {}
-    
-    # Create the full path of the folder
-    folder_path = os.path.join(os.getcwd(), folder_name)
-
-    # Get the list of the names of the Excel files in the folder
-    excel_list = glob.glob(os.path.join(folder_path, "*.xlsx"))
-
-    # Iterate over the list and apply the conversion function
-    for file in excel_list:
-        df_dict = excel_to_csv(file)
-        all_dfs.update(df_dict)
-        
-    # Move all .csv files to 'CSV' subfolder
-    move_csv_to_subfolder(folder_path)
-    
-    return all_dfs
-
-def main():
-    # Call the convert_excel_to_csv function with the desired folder name
-    convert_excel_to_csv("data")
+    Args:
+    - folder_path (str): The path to the folder containing Excel files to convert.
+    """
+    # Iterate over all Excel files in the specified folder and its subdirectories
+    for excel_path in glob.glob(os.path.join(folder_path, '**', '*.xlsx'), recursive=True):
+        # Convert each Excel file to CSV files (one per sheet)
+        excel_to_csv(excel_path)
 
 if __name__ == "__main__":
-    main()
+    # Sample execution
+    path_to_folder = "/path/to/your/folder"
+    folder_to_csv(path_to_folder)
--- excel_to_csv.py
+++ excel_to_csv.py
@@ -3,28 +3,46 @@ import os
 
 def excel_to_csv(excel_path):
     """
-    This function reads an Excel file and exports each sheet as a CSV file.
-    The CSV file name is the same as the sheet name.
-    
-    Parameters:
-        excel: a string with the Excel file name
-    
-    Return:
-         - dict: A dictionary where keys are sheet names and values are corresponding dataframes.
+    Convert sheets from an Excel file to CSV files. The CSV files are saved in a backup folder
+    structure under "CSV's_backout" which mirrors the original directory structure.
+
+    Args:
+    - excel_path (str): The path to the Excel file to convert.
+
+    Returns:
+    - dict: A dictionary where keys are CSV filenames (without '.csv' extension) 
+            and values are corresponding dataframes.
     """
-    # Read the Excel file as a dictionary of data frames
+    # Extract directory and filename from the provided path
+    directory, filename = os.path.split(excel_path)
+    base_filename = os.path.splitext(filename)[0]
+    
+    # Define backup directory for CSVs
+    backup_directory = os.path.join(directory, "CSV's_backout", base_filename)
+    if not os.path.exists(backup_directory):
+        os.makedirs(backup_directory)
+    
+    # Convert each sheet in the Excel file to a DataFrame and save as CSV
     df_dict = pd.read_excel(excel_path, sheet_name=None)
-    
-    # Get the directory path of the Excel file
-    directory = os.path.dirname(excel_path)
-    
     for sheet_name, df in df_dict.items():
-        # Create a full path for the CSV file to save in the same directory as the Excel file
-        csv_file = os.path.join(directory, sheet_name + ".csv")
-        try:
-            df.to_csv(csv_file, index=False, header=True)
-            print(f"Successfully exported {csv_file}")
-        except Exception as e:
-            print(f"Failed to export {csv_file}: {e}")
-            
+        csv_file_name = f"{base_filename}_{sheet_name}.csv"
+        version_suffix = 1
+        original_csv_file_name = csv_file_name
+        
+        # Check if file already exists, if so add a version suffix
+        while os.path.exists(os.path.join(backup_directory, csv_file_name)):
+            version_suffix += 1
+            csv_file_name = f"{original_csv_file_name[:-4]}_V{version_suffix}.csv"
+        
+        # Save the DataFrame as a CSV file in the backup directory
+        df.to_csv(os.path.join(backup_directory, csv_file_name), index=False, header=True)
+        
+        # Update dictionary key to match the CSV filename
+        df_dict[csv_file_name[:-4]] = df_dict.pop(sheet_name)
+
     return df_dict
+
+if __name__ == "__main__":
+    # Sample execution
+    path_to_excel = "sample.xlsx"
+    dfs = excel_to_csv(path_to_excel)
